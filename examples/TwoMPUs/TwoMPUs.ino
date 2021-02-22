649337a47d7e010acf4d950755f0f8b99f8961f4
Jefferson Lopes <55455265+Jefferson-Lopes@users.noreply.github.com>
2021-02-22 17:28:23 -0300

    update example

--- examples/TwoMPUs/TwoMPUs.ino
+++ examples/TwoMPUs/TwoMPUs.ino
@@ -19,12 +19,12 @@ void setup() {
   
   byte status1 = mpu1.begin();
   Serial.print(F("MPU6050 1 status: "));
-  Serial.println(status1 ? "ERROR " + status1 : "initialized successfully");
+  Serial.println(status1 ? "ERROR " + String(status1) : "initialized successfully");
   while(status1!=0){ } // stop everything if could not connect to MPU6050 1
 
   byte status2 = mpu2.begin();
   Serial.print(F("MPU6050 2 status: "));
-  Serial.println(status2 ? "ERROR " + status2 : "initialized successfully");
+  Serial.println(status2 ? "ERROR " + String(status2) : "initialized successfully");
   while(status2!=0){ } // stop everything if could not connect to MPU6050 2
   
   Serial.println(F("Calculating offsets, do not move MPU6050 1"));
